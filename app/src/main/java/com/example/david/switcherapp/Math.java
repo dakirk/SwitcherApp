d89eba97d1736409cc808d85ef17e53974034869
David Kirk <dsak@bu.edu>
2017-11-17 16:26:53 -0500

    Added Math.java for backend

---
+++ app/src/main/java/com/example/david/switcherapp/Math.java
@@ -0,0 +1,10 @@
+package com.example.david.switcherapp;//Really simple class to add 2 ints
+
+public class Math {
+    public Math() {
+    }
+
+    public int add(int i1, int i2) {
+        return (i1 + i2);
+    }
+}
