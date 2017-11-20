81355536351f65e8d62077c43285b972f7d4701c
David Kirk <dsak@bu.edu>
2017-11-20 15:14:57 -0500

    Added CartPoint and GameObject for backend (again)

--- app/src/main/java/com/example/david/switcherapp/MainActivity.java
+++ app/src/main/java/com/example/david/switcherapp/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //comment
+    //comment again
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
