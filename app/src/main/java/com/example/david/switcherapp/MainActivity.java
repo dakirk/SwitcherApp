2036a9a506aed8a8ac21efe5bb4e85a0036e2b59
David Kirk <dsak@bu.edu>
2017-12-03 00:38:21 -0500

    Added sprites for holes, and upscaled the sprites for all orcs, wizards, walls, and the start and level select buttons.

--- app/src/main/java/com/example/david/switcherapp/MainActivity.java
+++ app/src/main/java/com/example/david/switcherapp/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapFactory.Options;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
        // Math testImportedObject = new Math();
     }
